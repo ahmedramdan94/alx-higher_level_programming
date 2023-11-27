071a4f04d8e9d09d185447aab02c5ef3598843bb
ahmedramdan94 <bezorezo58@gmail.com>
2023-11-27 05:18:23 -0800

    2 task

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+'''
+Rectangle class.
+the module had class Rectangle
+use cases :
+new_rectangle = Rectangle()
+'''
+
+
+from hight import hight
+
+
+class Rectangle:
+    '''define class and his own attribute and methods'''
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = hight
+
+    @property
+    def width(self):
+        '''
+        width getter for private
+        Return:
+        self.width
+        '''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''
+        set width after git it
+        Return:
+        self.width = value
+         '''
+        if not isinstance(value, int):
+            raise TypeError('width must be integer')
+        if value < 0:
+            raise ValueError('no valid size value')
+            self.__width = value
+
+    @property
+    def height(self):
+        '''
+        hight getter for private
+        Return:
+        self.h
+        '''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''
+        set height after git it
+        Return:
+        self.height = value
+    '''
+        if not isinstance(value, int):
+            raise TypeError(' height must be integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
